3d44aff5bf62c054edadac5cd2163e14a3ade19f
sohilymansuri <57456481+sohilymansuri@users.noreply.github.com>
2020-11-22 02:44:40 +0530

    E

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -130,7 +130,7 @@ jQuery(document).ready(function ($) {
         console.log('error', e);
         $("#sendmessage").removeClass("show");
         $("#errormessage").addClass("show");
-        $('#errormessage').html(e);
+        $('#errormessage').html(e.message);
       }
     });
 
