e96a2da8c80d131e4abb91ce803a2cd0cff4343b
sohil <lislife462@gmail.com>
2020-11-22 08:13:32 +0530

    link

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -58,10 +58,8 @@ jQuery(document).ready(function ($) {
       }
     });
     f.children('textarea').each(function () { // run all inputs
-
       var i = $(this); // current input
       var rule = i.attr('data-rule');
-
       if (rule !== undefined) {
         var ierror = false; // error flag for current input
         var pos = rule.indexOf(':', 0);
@@ -92,30 +90,9 @@ jQuery(document).ready(function ($) {
     var action = $(this).attr('action');
     console.log("str", str);
     if (!action) {
-      action = "http://127.0.0.1:8080/msg_me"
-      // action = 'https://node-sohil-ga.herokuapp.com/msg_me';
+      // action = "http://127.0.0.1:8080/msg_me"
+      action = 'https://node-sohil-ga.herokuapp.com/msg_me';
     }
-
-
-    // $.ajax('http://localhost:3000/msg_me', {
-    //   type: 'POST',
-    //   data: JSON.stringify(data),
-    //   contentType: 'application/json',
-    //   success: function (s) {
-    //     $("#sendmessage").addClass("show");
-    //     $("#errormessage").removeClass("show");
-    //     $('.contactForm').find("input, textarea").val("");
-    //     console.log('success', s);
-    //   },
-    //   error: function (e) {
-    //     console.log('error', e);
-    //     $("#sendmessage").removeClass("show");
-    //     $("#errormessage").addClass("show");
-    //     $('#errormessage').html(e);
-    //   }
-    // });
-
-
     $.ajax({
       type: 'GET',
       data: str,
@@ -134,10 +111,6 @@ jQuery(document).ready(function ($) {
         $('#errormessage').html(e.message);
       }
     });
-
-
-
-
     return false;
   });
 
